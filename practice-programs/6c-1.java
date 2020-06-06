7a7225aa22a64d7ef31e1a0215496ec51439ac69
saikailash1 <60144157+saikailash1@users.noreply.github.com>
2020-06-06 18:22:04 +0530

    Create 6c-1.java

---
+++ practice-programs/6c-1.java
@@ -0,0 +1,26 @@
+//High School app
+public class Person //Parent Class
+{
+	String name;
+	int age;
+	String qualification;
+	public Person()
+	{
+		name = "Steve Rogers";
+		age = 45;
+		qualification = "B.E";
+	}
+}
+
+class Student//Child Class
+{
+	String name;
+	int age;
+	String School;
+	public Student()
+	{
+		name = "Bucky Barnes";
+		age  = 20;
+		School = "Queens School";
+	}
+}
