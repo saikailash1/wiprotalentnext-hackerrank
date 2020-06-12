777ef25445a65fa0bef65cf8c98a10bd054295ef
saikailash1 <60144157+saikailash1@users.noreply.github.com>
2020-06-12 17:57:50 +0530

    Create talentnext13.java

---
+++ talentnext13.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class code{
+public static void main(String args[])
+{
+Scanner sc = new Scanner(System.in)
+int a = sc.next();
+String b = Integer.toString(a);
+int n = b.length();
+int path = 0;
+char[] ch = new char[n];
+for(int i=0;i<n;i++)
+{
+char[i] = b.charAt(i);
+}
+for(int i=0;i<n;i++)
+{
+
+if(ch[i]=='0')
+{
+path = path+1;
+}
+
+else if(ch[i]=='8')
+{
+path = path+2;
+}
+
+else if(ch[i]=='4')
+{
+path = path+1;
+}
+
+else if(ch[i]=='6')
+{
+path = path+1;
+}
+
+else if(ch[i]=='8')
+{
+path = path+1;
+}
+
+else
+{
+continue;
+}
+}
+System.out.println(path);
+}
+}
