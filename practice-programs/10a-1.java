36cd997425235c4150988e428f16203510f6fce2
saikailash1 <60144157+saikailash1@users.noreply.github.com>
2020-06-13 19:26:08 +0530

    Create 10a-1.java

---
+++ practice-programs/10a-1.java
@@ -0,0 +1,16 @@
+package test;
+
+public class Foundation
+{
+	private int var1 = 3;
+	int var2 = 4;
+	protected int var3 = 5;
+	public int var4 = 6;
+	public void print()
+	{
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var3);
+		System.out.println(var4);
+	}
+}
