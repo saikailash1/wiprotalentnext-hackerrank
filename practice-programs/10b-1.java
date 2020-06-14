0e4b8ccd2fe9da0b217d74fb3495c129aa7682ce
saikailash1 <60144157+saikailash1@users.noreply.github.com>
2020-06-14 12:21:37 +0530

    Create 10b-1.java

---
+++ practice-programs/10b-1.java
@@ -0,0 +1,11 @@
+package com.wipro.automobile.ship;
+public class Compartment
+{
+	int height = 90;
+	int width = 69;
+	int breadth = 80;
+	public void watertight()
+	{
+		System.out.println(height+" "+width+" "+breadth);
+	}
+}
