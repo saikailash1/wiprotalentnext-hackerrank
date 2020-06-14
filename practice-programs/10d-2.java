0334491cdfefd67a301063cfd9cfd3a19703f93b
saikailash1 <60144157+saikailash1@users.noreply.github.com>
2020-06-14 13:26:04 +0530

    Create 10d-2.java

---
+++ practice-programs/10d-2.java
@@ -0,0 +1,29 @@
+package FourWheeler;
+import com.automobile.*;
+public class Ford extends Vehicle
+{
+	public String getModelName()
+	{
+		return "Ford GT Mustang";
+	}
+	
+	public String getRegistrationNumber()
+	{
+		return "AP-09-1218";
+	}
+	
+	public String getOwnerName()
+	{
+		return "Dominic Toretto";
+	}
+	
+	public int speed()
+	{
+		return 95;
+	}
+	
+	public int tempControl()
+	{
+		return 23;
+	}
+}
